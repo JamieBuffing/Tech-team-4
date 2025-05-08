5d8ec8801d37865896b1bb0779c92a7834b70a3b
Jamie <jamiebuffing@gmail.com>
2025-05-08 14:22:44 +0200

    lint updates

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,14 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
+  {
+		rules: {
+			"no-unused-vars": "warn",
+			"no-undef": "warn",
+		},
+	},
 ]);
+
+import { defineConfig } from "eslint/config";
+import js from "@eslint/js";
+	
