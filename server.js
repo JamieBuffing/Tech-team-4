4271473b50a4fe2fdb4f99807d1be661ede13f84
Jamie <jamiebuffing@gmail.com>
2025-05-08 15:31:29 +0200

    Test commit with mongodb

--- server.js
+++ server.js
@@ -1,1 +1,24 @@
-console.log("Hallo wereld!");
+require('dotenv').config()
+const { MongoClient, ObjectId } = require("mongodb");
+
+const uri = process.env.URI;
+
+const client = new MongoClient(uri);
+const db = client.db(process.env.DB);
+
+async function connectDB() {
+    try {
+        await client.connect();
+        console.log("Client connected to database");
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+connectDB()
+
+
+
+app.listen(3000);
+console.log('Server is listening on port 3000');
+console.log('http://localhost:3000/')
